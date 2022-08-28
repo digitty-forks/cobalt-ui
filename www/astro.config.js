69a315e18153da0e34954f8a0f579ba59476a6a6
Drew Powers <drew@pow.rs>
2022-08-28 09:56:54 -0600

    switch to GitHub Pages, fix lint error

--- www/astro.config.js
+++ www/astro.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [vue()],
-  site: `http://cobalt-ui.pages.dev`,
+  site: `https://drwpow.github.io`,
+  base: '/cobalt-ui',
 });
